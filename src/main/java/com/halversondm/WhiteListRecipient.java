fcc0afbff343ba54cf8e1e02a0a9fc4a436497c5
Dan Halverson <daniel.m.halverson@gmail.com>
2018-03-16 06:43:48 -0500

    initial commit

---
+++ src/main/java/com/halversondm/WhiteListRecipient.java
@@ -0,0 +1,46 @@
+package com.halversondm;
+
+import org.apache.mailet.MailAddress;
+import org.apache.mailet.base.GenericRecipientMatcher;
+
+import javax.mail.MessagingException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WhiteListRecipient extends GenericRecipientMatcher {
+
+    List<String> emailNames = new ArrayList<>();
+
+    public WhiteListRecipient() {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("email_whitelist.txt");
+        if (inputStream != null) {
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (!line.startsWith("#") && !line.isEmpty()) {
+                        emailNames.add(line);
+                    }
+                }
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            throw new RuntimeException("The email_whitelist.txt file on the classpath is necessary to use this Matcher");
+        }
+    }
+
+    public boolean matchRecipient(MailAddress mailAddress) throws MessagingException {
+        String localPart = mailAddress.getLocalPart();
+        return emailNames.contains(localPart);
+    }
+}
---
+++ src/test/java/com/halversondm/WhiteListRecipientTest.java
@@ -0,0 +1,33 @@
+package com.halversondm;
+
+import org.apache.mailet.MailAddress;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WhiteListRecipientTest {
+
+    WhiteListRecipient unit;
+
+    @Test
+    public void constructor_WhenTypical() {
+        unit = new WhiteListRecipient();
+        Assert.assertFalse(unit.emailNames.isEmpty());
+        Assert.assertEquals(4, unit.emailNames.size());
+    }
+
+    @Test
+    public void matchReceipient_WhenTypical() throws Exception {
+        unit = new WhiteListRecipient();
+        MailAddress mailAddress = new MailAddress("daniel.m.halverson@gmail.com");
+        boolean isValid = unit.matchRecipient(mailAddress);
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void matchReceipient_WhenNotFound() throws Exception {
+        unit = new WhiteListRecipient();
+        MailAddress mailAddress = new MailAddress("spot.doe@gmail.com");
+        boolean isValid = unit.matchRecipient(mailAddress);
+        Assert.assertFalse(isValid);
+    }
+}
